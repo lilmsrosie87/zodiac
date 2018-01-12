80412de6b638df407d3eaf93c4ad8c3953ab034a
Roslyn Harris <rharris0113@gmail.com>
2018-01-11 21:44:47 -0800

    working on zodiac array in javascript

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function myZodiac {
+	var userInput = document.getElementById("astrology");
+	var text = "";
+	var i; 
+for (i = 0; i < userInput.length; i++) {
+	text += userInput.elements[i].value + "<br>";
+	}
+	document.getElementById("results").innerHTML = text;
+}
+
+var zodiac [
+{
+	zodiac: Aries;
+	sign: img/;
+	traits: "the best sign ever!";
+},
+{
+	zodiac: Capricorn;
+	sign: img/;
+	traits: "very stubborn";
+}
+{
+	zodiac: Piscis;
+	sign: img/;
+	traits: "goes with the flow... usually";
+}
