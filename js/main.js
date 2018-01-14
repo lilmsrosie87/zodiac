a8f211d8cccabd190ae4836f4471a5f86db60614
Roslyn Harris <rharris0113@gmail.com>
2018-01-14 01:31:59 -0800

    added array of zodiac signs

--- js/main.js
+++ js/main.js
@@ -2,25 +2,64 @@ function myZodiac {
 	var userInput = document.getElementById("astrology");
 	var text = "";
 	var i; 
+	
 for (i = 0; i < userInput.length; i++) {
 	text += userInput.elements[i].value + "<br>";
 	}
 	document.getElementById("results").innerHTML = text;
 }
 
+
+
 var zodiac [
 {
-	zodiac: Aries;
-	sign: img/;
-	traits: "the best sign ever!";
+	zodiac: "Aries",
+	sign: "img/Aries.jpg",
+	traits: "warrior, strong willied, caring, the bold ram is ready to take on any challenge",
 },
 {
-	zodiac: Capricorn;
-	sign: img/;
-	traits: "very stubborn";
+	zodiac: "Capricorn",
+	sign: "img/Capricorn.jpg",
+	traits: "the ambitous goat is alway on top",
 }
 {
-	zodiac: Piscis;
-	sign: img/;
-	traits: "goes with the flow... usually";
+	zodiac: "Pisces",
+	sign: "img/pisces.jpg",
+	traits: "the dreamy fish flow with the currents",
 }
+}
+	zodiac: :"Leo"
+	sign: "img/leo.jpg",
+	traits: "the generous lion lives to express itself",
+}
+{
+	zodiac: "Saggitarius",
+	sign: "img/saggitarius",
+	traits: "the far seeing archer is always expanding",
+}
+}
+	zodiac: "Taurus",
+	sign: "img/taurus.jpg"",
+	traits: "the determined bull knows that slow and steay wins the race",
+}
+{
+	zodiac: "Virgo",
+	sign: "img/virgo.jpg",
+	traits: "the discerning virgin strives for perfection",
+}
+{
+	zodiac: "Gemini",
+	sign: "img/gemini.jpg",
+	traits: "the quick thinking twins shake things ups",
+}
+{
+	zodiac: "Libra",
+	sign: "img/libra.jpg",
+	traits: "the worldy water barrier marches to its own beat",
+}
+{
+	zodiac: "Cancer",
+	sign: "img/cancer.jpg",
+	traits: "the sensitive carb loves to love others",
+}
+]
