5f35cb093a1bc1f58653710fe9128c9a6f5ec352
Roslyn Harris <rharris0113@gmail.com>
2018-01-14 16:24:38 -0800

    moved function below array to see if onclick will work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,70 @@
+// moved the function below zodiac array to see if this will work better
+
+// function myZodiac {
+	// var userInput = document.getElementById("astrology");
+	// var text = "";
+	// var i; 
+	
+// for (i = 0; i < userInput.length; i++) {
+	// text += userInput.elements[i].value + "<br>";
+	// }
+	// document.getElementById("results").innerHTML = text;
+// }
+
+// zodiac array below
+var zodiac = [
+{
+	zodiac: "Aries",
+	sign: "img/Aries.jpg",
+	traits: "warrior, strong willied, caring, the bold ram is ready to take on any challenge",
+},
+{
+	zodiac: "Capricorn",
+	sign: "img/Capricorn.jpg",
+	traits: "the ambitous goat is alway on top",
+},
+{
+	zodiac: "Pisces",
+	sign: "img/pisces.jpg",
+	traits: "the dreamy fish flow with the currents",
+},
+{
+	zodiac: "Leo",
+	sign: "img/leo.jpg",
+	traits: "the generous lion lives to express itself",
+},
+{
+	zodiac: "Saggitarius",
+	sign: "img/saggitarius",
+	traits: "the far seeing archer is always expanding",
+},
+{
+	zodiac: "Taurus",
+	sign: "img/taurus.jpg",
+	traits: "the determined bull knows that slow and steay wins the race",
+},
+{
+	zodiac: "Virgo",
+	sign: "img/virgo.jpg",
+	traits: "the discerning virgin strives for perfection",
+},
+{
+	zodiac: "Gemini",
+	sign: "img/gemini.jpg",
+	traits: "the quick thinking twins shake things ups",
+},
+{
+	zodiac: "Libra",
+	sign: "img/libra.jpg",
+	traits: "the worldy water barrier marches to its own beat",
+},
+{
+	zodiac: "Cancer",
+	sign: "img/cancer.jpg",
+	traits: "the sensitive carb loves to love others",
+}
+]
+
 function myZodiac {
 	var userInput = document.getElementById("astrology");
 	var text = "";
@@ -9,57 +76,3 @@ for (i = 0; i < userInput.length; i++) {
 	document.getElementById("results").innerHTML = text;
 }
 
-
-
-var zodiac [
-{
-	zodiac: "Aries",
-	sign: "img/Aries.jpg",
-	traits: "warrior, strong willied, caring, the bold ram is ready to take on any challenge",
-},
-{
-	zodiac: "Capricorn",
-	sign: "img/Capricorn.jpg",
-	traits: "the ambitous goat is alway on top",
-}
-{
-	zodiac: "Pisces",
-	sign: "img/pisces.jpg",
-	traits: "the dreamy fish flow with the currents",
-}
-}
-	zodiac: :"Leo"
-	sign: "img/leo.jpg",
-	traits: "the generous lion lives to express itself",
-}
-{
-	zodiac: "Saggitarius",
-	sign: "img/saggitarius",
-	traits: "the far seeing archer is always expanding",
-}
-}
-	zodiac: "Taurus",
-	sign: "img/taurus.jpg"",
-	traits: "the determined bull knows that slow and steay wins the race",
-}
-{
-	zodiac: "Virgo",
-	sign: "img/virgo.jpg",
-	traits: "the discerning virgin strives for perfection",
-}
-{
-	zodiac: "Gemini",
-	sign: "img/gemini.jpg",
-	traits: "the quick thinking twins shake things ups",
-}
-{
-	zodiac: "Libra",
-	sign: "img/libra.jpg",
-	traits: "the worldy water barrier marches to its own beat",
-}
-{
-	zodiac: "Cancer",
-	sign: "img/cancer.jpg",
-	traits: "the sensitive carb loves to love others",
-}
-]
